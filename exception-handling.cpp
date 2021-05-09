574cba78e2b0a7a9310c57f0f7b4bd69b733bfa6
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2021-05-09 22:36:30 +0530

    Update exception-handling.cpp

--- exception-handling.cpp
+++ exception-handling.cpp
@@ -1,27 +1,56 @@
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// int main(){
+// //Write your code here !
+
+// int age;
+// cout<<"Enter your Age"<<endl;
+// cin>>age;
+// try{
+//     if (age>=18)
+//     {
+//         /* code */
+//         cout<<"You are eligible "<<endl;
+//     }
+//     else{
+//         throw (505);
+//     }
+    
+// }
+// catch(int num){
+//     cout<<"not eligible"<<endl;
+//     cout<<"Error:"<<num;
+// }
+// }
+
+//Hi Cypher 
+//Happy CODING :)
 #include <bits/stdc++.h>
 using namespace std;
 
 
 int main(){
 //Write your code here !
+int num1,num2;
+cout<<"Enter two numbers :: ";
+cin>>num1>>num2;
 
-int age;
-cout<<"Enter your Age"<<endl;
-cin>>age;
+int sum=num1+num2;
+
 try{
-    if (age>=18)
-    {
-        /* code */
-        cout<<"You are eligible "<<endl;
-    }
-    else{
-        throw (505);
-    }
-    
+
+  if(sum>=200){
+      cout<<"Passed";
+  }
+  else{
+      throw(505);
+  }
 }
 catch(int num){
-    cout<<"not eligible"<<endl;
-    cout<<"Error:"<<num;
+    cout<<"Error "<<num;
 }
+
 }
